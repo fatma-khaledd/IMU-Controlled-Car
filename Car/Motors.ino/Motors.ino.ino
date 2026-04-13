abb72c17dfc946de6ac0643ac184b39193678072
saifullah <seif3sc@gmail.com>
2026-04-13 15:12:12 +0200

    forward and right function

---
+++ Car/Motors.ino/Motors.ino.ino
@@ -0,0 +1,51 @@
+#define in1 1
+#define in2 2
+#define in3 3
+#define in4 4
+#define en1 5
+#define en2 6
+
+void forward(float varPitch);//pitch
+void right(float varRoll);//roll
+
+void setup()) 
+{
+pinMode(in1,OUTPUT);
+pinMode(in2,OUTPUT);
+pinMode(in3,OUTPUT);
+pinMode(in4,OUTPUT);
+pinMode(en1,OUTPUT);
+pinMode(en2,OUTPUT);
+}
+
+float pitch, roll, varPitch, varRoll;
+
+void loop()
+{
+
+    varPitch = map(pitch,0,90,0,255);
+    varRoll = map(Roll,0,90,0,255);
+
+    if (abs(pitch) > 5 && abs(pitch) < 90)
+        forward();
+
+    if (abs(roll) > 5 && abs(roll) < 90)
+        right();    
+}
+
+void forward(float varPitch)
+{
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+    analogWrite(en1, varPitch);
+}
+void right(float varRoll)
+{
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+    analogWrite(en1, varRoll);
+
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, HIGH);
+    analogWrite(en2, varRoll/2);
+}
